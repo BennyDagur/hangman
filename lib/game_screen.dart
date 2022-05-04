87500567ca34f4b2ad9b746b2f7d79931c50b91b
BennyDagur <bdg2@nemandi.ntv.is>
2022-05-04 21:31:31 +0000

    Possible placement of parts

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -9,7 +9,6 @@
 
 //TODO Place for the hangman art - which will update as the game progresses (with each failure)
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
@@ -23,6 +22,35 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hangman'),
+      ),
+      body: Row(
+        children: [
+          //Place for the hanged man
+              Expanded(
+                child: Container(
+                  child: Icon(Icons.email),
+                  color: Colors.yellow,
+                ),
+              ),
+          Column(
+            children: [
+              //Show what they have gotten and how many letters there are
+              Expanded(
+                child: Container(
+                  child: Icon(Icons.check_rounded),
+                  color: Colors.red,
+                  ),
+              ),
+              //A bin for the Incorrect letters Inputted
+              Expanded(
+                child: Container(
+                  child: Icon(Icons.close),
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
